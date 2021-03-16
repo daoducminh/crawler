e415faa0aa3092250d2b97da4969c675aaf481b3
daoducminh <daoducminh1997@gmail.com>
2021-03-16 09:47:21 +0700

    Update script

--- crawl_food.sh
+++ crawl_food.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# dt=$(date '+%Y_%m_%d_%H_%M_%S')
-# mkdir -p /content/gdrive/MyDrive/Projects/it5230/$dt
-.venv/bin/python -m scrapy crawl food -a number=$1 
-# && cp -rf food/* /content/gdrive/MyDrive/Projects/it5230/
+dt=$(date '+%Y_%m_%d_%H_%M_%S')
+mkdir -p /content/gdrive/MyDrive/Projects/it5230/$dt
+.venv/bin/python -m scrapy crawl food -a number=$1 && cp -rf food/* /content/gdrive/MyDrive/Projects/it5230/
