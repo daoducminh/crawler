b6bc1bbe5adc941d2e5c7c3a1a600eb225da2ace
Minh Dao <daoducminh1997@gmail.com>
2021-04-05 10:48:56 +0700

    Update crawl_food.sh

--- crawl_food.sh
+++ crawl_food.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 dt=$(date '+%Y_%m_%d_%H_%M_%S')
-mkdir -p /content/gdrive/MyDrive/Projects/it5230/$dt
+mkdir -p /content/drive/MyDrive/Projects/it5230/$dt
 .venv/bin/python -m scrapy crawl food -a number=$1 && cp -rf food/* /content/drive/MyDrive/Projects/it5230/$dt/
